52c0958a0432d070922ec8d46426765908a550d9
Benjamin Yates <benjamin@rqdq.com>
2016-05-07 12:02:31 -0400

    forgotten __init__ import
    also, version bump

--- urwid_pydux/__init__.py
+++ urwid_pydux/__init__.py
@@ -1,3 +1,5 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
-from .urwid_pydux import Component, subscribe_urwid_redraw
+from .urwid_pydux import (
+    Component, ConnectedComponent, subscribe_urwid_redraw,
+)
